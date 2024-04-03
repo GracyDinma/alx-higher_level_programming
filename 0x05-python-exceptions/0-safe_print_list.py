0345cedac511e21e7b87616681e7b898042a9ec5
GracyDinma <okohagchidinma@gmail.com>
2024-04-03 06:30:54 -0700

    printlist

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,6 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     for element in range(x):
         try:
-            my_list[]
             print(my_list[element])
             count += element
         except IndexError:
