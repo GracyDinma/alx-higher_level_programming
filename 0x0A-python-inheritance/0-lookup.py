a7c4fabd07c2affbb5df80fb210d5f06839f2e69
GracyDinma <okohagchidinma@gmail.com>
2024-04-17 01:02:52 -0700

    lookup obj

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+A functions that returns the list of available attribute and methods
+"""
+
+def lookup(obj):
+
+    """
+    Args
+    returns a list object
+    """
+
+    return dir(obj)
