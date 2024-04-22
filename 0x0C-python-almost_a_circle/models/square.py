85bee85887220d4e628cdc911a6804285f5f7ec1
GracyDinma <okohagchidinma@gmail.com>
2024-04-21 19:32:11 -0700

    python file

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -40,3 +40,20 @@ class Square(Rectangle):
                 self.id, self.__x, self.__y, self.__width)
 
         return s
+
+    @property
+    def size(self):
+        """Retrieves the size attribute."""
+
+        return self.__width
+
+    @size.setter
+    def size(self, value):
+        """Sets the size of the attribute"""
+
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+        self.__height = value
