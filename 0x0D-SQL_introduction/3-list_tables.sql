eb1a2ffc7c3317bd9b88d7b21e1b6820982b4446
GracyDinma <okohagchidinma@gmail.com>
2024-05-22 10:12:31 -0700

    Task3

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,5 @@
+-- lists all the tables of a database in your MySQl server.
+-- The database name will be passed as argument of myaql command
+-- in the following example: mysql is the name of the database.
+
+SHOW TABLE:S; 
