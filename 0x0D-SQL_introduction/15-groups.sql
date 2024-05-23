0f4f39512b2015acba6e9531465fc77192cfc757
GracyDinma <okohagchidinma@gmail.com>
2024-05-23 04:38:34 -0700

    updated

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,12 @@
+-- lists the number of records with the same score in the table second_table
+-- of the database hbtn_0c_0 in your MySQL server.
+-- The result should display:
+-- the score
+-- the number of records for this score with the label number
+-- the list should be sorted by the number of records(descending)
+-- the database name will be passed as an argument to hte mysql command
+
+SELECT score, COUNT(*) As number
+from second_table
+GROUP BY score
+ORDER BY number DESC;
