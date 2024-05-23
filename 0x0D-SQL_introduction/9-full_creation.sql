f5173ad13c9e0a11bbb51cd0fda06285070ee007
GracyDinma <okohagchidinma@gmail.com>
2024-05-23 02:06:07 -0700

    Task 9

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,18 @@
+-- creates a table second_table in the datbase hbtn_0c_0 in your MySQL server
+-- and add multiples rows.
+-- second_table description:
+-- id INT
+-- name VARCHAR(256)
+-- score INT
+-- The databse name will be passed as an argument to the mysql command
+-- if the table second_table already exists, your script should not fail
+-- You are not allowed to use he SELECT and SHOW statements
+-- Your script should create these records:
+-- id = 1, name = "John", score = 10
+-- id = 2, name = "Alex", score = 3
+-- id = 3, name = "Bob", score = 14
+-- id = 4, name = "George", score = 8
+
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (id, name, score)
+VALUES (1, "John", 10), (2, "Alex", 3), (3,  "Bob", 14), (4, "George", 18);
