5c9ccc60eedc15c51be9e307e6e3ce9cdab16ee2
GracyDinma <okohagchidinma@gmail.com>
2024-05-23 19:55:46 -0700

    Task 6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,5 @@
--- creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa)on your MySQL server.
+-- creates the database hbtn_0d_usa and the table states
+-- (in the database hbtn_0d_usa)on your MySQL server.
 -- states description: id Int unique, auto generated, name VARCHAR(256) == null
 -- If the database hbtn_0d_usa already exists, your script should not fail
 -- If the table states already exists, your script should not fail
