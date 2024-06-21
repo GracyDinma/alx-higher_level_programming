f6509f21e6f9b45b7a98d026c31ee02f0809111f
GracyDinma <okohagchidinma@gmail.com>
2024-06-20 19:27:25 -0700

    task 2 fixed

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,11 +3,11 @@
  * Rectangle that defines rectangle width and height
  */
 class Rectangle {
-	constructor(w, h) {
-		this.width = w;
-		this.height = h;
-	}
-	if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
-	}
+  constructor (w, h) {
+    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
 }
 module.exports = Rectangle;
