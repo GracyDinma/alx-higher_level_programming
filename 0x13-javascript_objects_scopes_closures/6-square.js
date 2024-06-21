20f9efbe2c2da1872fdcd63035332554a630a0c9
GracyDinma <okohagchidinma@gmail.com>
2024-06-20 21:31:05 -0700

    task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,3 +2,20 @@
 /*
  * A square that inherits from Square
  */
+const PrevSquare = require('./5-square');
+
+class Square extends PrevSquare {
+  charPrint (c) {
+    const myChar = c === undefined ? 'X' : c;
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += myChar;
+      }
+
+      console.log(row);
+    }
+  }
+}
+
+module.exports = Square;
