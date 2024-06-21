e3b6c1e6a0fb5959f05b85bec382f7e93086f7e7
GracyDinma <okohagchidinma@gmail.com>
2024-06-20 19:19:46 -0700

    task 2

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/*
+ * Rectangle that defines rectangle width and height
+ */
+class Rectangle {
+	constructor(w, h) {
+		this.width = w;
+		this.height = h;
+	}
+	if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
+	}
+}
+module.exports = Rectangle;
