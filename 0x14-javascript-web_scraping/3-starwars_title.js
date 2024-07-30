f1c15cc738621c7af447ca76dbf9e6feb5fb9b0e
GracyDinma <okohagchidinma@gmail.com>
2024-07-30 16:24:07 -0700

    task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const request = require('request');
-const starWarsUrl = 'http://swapi-api.hbtn.io/api/films/'.concat(process.argv[2];
+const starWarsUrl = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
 
 request(starWarsUrl, function (_err, _res, body) {
   body = JSON.parse(body);
